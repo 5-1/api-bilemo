a70646b46cea873af6f5922a44b7aab90c54a547
5-1 <shark51LoL@hotmail.fr>
2020-04-22 00:57:24 +0200

    change offset for page and add serializerGroupe "list"

--- src/Representation/Customers.php
+++ src/Representation/Customers.php
@@ -2,24 +2,32 @@
 
 namespace App\Representation;
 
+use JMS\Serializer\Annotation as Serializer;
 use Pagerfanta\Pagerfanta;
 use JMS\Serializer\Annotation\Type;
 class Customers
 {
     /**
-     * @Type("array<App\Entity\Phone>")
+     * @Type("array<App\Entity\Customer>")
+     * @Serializer\Groups({"list"})
      */
     public $data;
+    /**
+     * @var
+     * @Serializer\Groups({"list"})
+     */
     public $meta;
 
     public function __construct(Pagerfanta $data)
     {
-        $this->data = $data;
+        $this->data = $data->getCurrentPageResults();
+
+
 
         $this->addmeta('limit', $data->getMaxPerPage());
         $this->addMeta('current_items', count($data->getCurrentPageResults()));
         $this->addMeta('total_items', $data->getNbResults());
-        $this->addMeta('offset', $data->getCurrentPageOffsetStart());
+        $this->addMeta('offset', $data->getCurrentPage());
     }
     public function addMeta($name, $value)
     {
