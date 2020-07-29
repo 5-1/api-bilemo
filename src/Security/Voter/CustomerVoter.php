fbfb87cbba60ceb8aa86bfedaab09592324ef9d3
Johan B <31527530+5-1@users.noreply.github.com>
2020-07-29 23:11:17 +0200

    forget file on git  (#19)
    * Create first FunctionalTest with createAuthenticatedUser for log in.
    
    * Create AuthenticatedTrait.php
    
    * Create CustomerControllerTest for testlist, testshow, testForbiddenShow, testnew, testdetele and testDeleteReturn403ForUnauthorizedAccess
    
    * update CustomerControllerTest:
    use AuthentificatedTrait,
    add smokeTest for all url
    
    * Create PhoneControllerTest.php for testlist, testshow (all the url of the phone entry)
    
    * Add DAMADoctrineTestBundle for test (automatic restauration of the database)
    
    * Add DAMADoctrineTestBundle and reset script:
    force reset database and load all fixture with hautelook !
    
    * use Hateoas\Configuration and Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted
    
    * delete naming_strategy: doctrine.orm.naming_strategy.underscore in doctrine.yaml
    add Exception\ResourceValidationException 400 in fos_rest.yaml
    delete no use in routes.yaml
    
    add Dama in phpunit.xml.dist
    add Blackfire in symfony.lock and composer.lock
    
    * forget file on git !
    nelmio_api_doc.yaml for config for nelmio;
    ResourceValidationException.php;
    CustomerVoter.php
    and bootstrap.php for test
    without this file, we can't download and test the project, now is done :)

---
+++ src/Security/Voter/CustomerVoter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Customer;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class CustomerVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['Delete','Show'])
+            && $subject instanceof Customer;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'Delete':
+                return $user === $subject->getUser();
+                break;
+            case 'Show':
+                return $user === $subject->getUser();
+                break;
+        }
+
+        return false;
+    }
+}
